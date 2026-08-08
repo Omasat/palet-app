f9ac692f231333a5f02eab1f15f1bdf4fac07e87
Omasat <omasat@users.noreply.github.com>
2026-08-08 19:26:15 +0300

    Start native PHP session for SessionGuard

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+session_start();
+
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(\Palet\Framework\Contracts\Http\KernelInterface::class);
